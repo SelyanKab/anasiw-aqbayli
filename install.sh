585dba7d5dfeae7d21b310147078b5e4bbbdb160
Selyan Slimane Amiri <selyan.kab@gmail.com>
2020-10-28 17:31:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,25 @@ echo ""
 echo "-------- Asebded n unasiw aqbayli -----------------"
 echo "-------- installation du clavier kabyle -----------"
 echo " I tallelt:"
-echo " Besoin d'aide: https://github.com/S-Amiri/Anasiw-aqbayli-i-Linux"
+echo " Besoin d'aide: https://github.com/SelyanKab/anasiw-aqbayli"
 echo ""
 echo "---------------------------------------------------"
 
 sudo mkdir /usr/share/X11/xkb/rules/tmp 
+
+sudo mv /usr/share/X11/xkb/rules/base.lst /usr/share/X11/xkb/rules/tmp
+sudo mv /usr/share/X11/xkb/rules/base.xml /usr/share/X11/xkb/rules/tmp
+sudo mv /usr/share/X11/xkb/rules/evdev /usr/share/X11/xkb/rules/tmp
+sudo mv /usr/share/X11/xkb/rules/evdev.lst /usr/share/X11/xkb/rules/tmp
 sudo mv /usr/share/X11/xkb/rules/evdev.xml /usr/share/X11/xkb/rules/tmp
+
+sudo cp base.lst /usr/share/X11/xkb/rules
+sudo cp base.xml /usr/share/X11/xkb/rules
+sudo cp evdev /usr/share/X11/xkb/rules
+sudo cp evdev.lst /usr/share/X11/xkb/rules
 sudo cp evdev.xml /usr/share/X11/xkb/rules
-sudo cp kabyle /usr/share/X11/xkb/symbols
+
+sudo cp kab /usr/share/X11/xkb/symbols
 
 echo "Asebded immed"
 echo "Installation términée"
